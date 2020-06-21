504e7c947242ae3108f316e65252e9622e345956
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-06-21 14:20:45 +0800

    feat: add text.go

---
+++ render/text_test.go
@@ -0,0 +1,39 @@
+package render
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRenderString(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	(String{
+		Format: "hello %s %d",
+		Data:   []interface{}{},
+	}).WriteContentType(w)
+	assert.Equal(t, "text/plain; charset=utf-8", w.Header().Get("Content-Type"))
+
+	err := (String{
+		Format: "hola %s %d",
+		Data:   []interface{}{"manu", 2},
+	}).Render(w)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "hola manu 2", w.Body.String())
+	assert.Equal(t, "text/plain; charset=utf-8", w.Header().Get("Content-Type"))
+}
+
+func TestRenderStringLenZero(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	err := (String{
+		Format: "hola %s %d",
+		Data:   []interface{}{},
+	}).Render(w)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "hola %s %d", w.Body.String())
+	assert.Equal(t, "text/plain; charset=utf-8", w.Header().Get("Content-Type"))
+}
