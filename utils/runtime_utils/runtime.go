75e998d56e17c72be1cfb2c29d57e65d8a43342d
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-06-24 21:39:17 +0800

    feat: add runtime.go

---
+++ utils/runtime_utils/runtime.go
@@ -0,0 +1,13 @@
+package runtime_utils
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// ConfigRuntime sets the number of operating system threads.
+func ConfigRuntime() {
+	nuCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(nuCPU)
+	fmt.Printf("Running with %d CPUs\n", nuCPU)
+}
