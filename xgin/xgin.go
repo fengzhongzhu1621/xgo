9578190d8ca07c2ebf5d4723609b94d83adf6220
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-06-26 21:32:00 +0800

    feat: change xgin.go

--- xgin/xgin.go
+++ xgin/xgin.go
@@ -1,13 +1,1 @@
 package xgin
-
-// Default returns an Engine instance with the Logger and Recovery middleware already attached.
-// 模块方法
-func Default() *Engine {
-	debugPrintWARNINGDefault()
-	// 创建一个engine对象
-	engine := New()
-	// 加载中间件
-	engine.Use(Logger(), Recovery())
-	return engine
-}
-
