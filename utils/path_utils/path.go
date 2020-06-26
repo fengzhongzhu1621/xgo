ece2b48decaa3c17c4bc54a5ea2879fa12644d43
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-06-26 19:57:59 +0800

    feat: change utils

---
+++ utils/path_utils/path.go
@@ -0,0 +1,22 @@
+package path_utils
+
+import "path"
+
+func lastChar(str string) uint8 {
+	if str == "" {
+		panic("The length of the string can't be 0")
+	}
+	return str[len(str)-1]
+}
+
+func JoinPaths(absolutePath, relativePath string) string {
+	if relativePath == "" {
+		return absolutePath
+	}
+
+	finalPath := path.Join(absolutePath, relativePath)
+	if lastChar(relativePath) == '/' && lastChar(finalPath) != '/' {
+		return finalPath + "/"
+	}
+	return finalPath
+}
--- utils/runtime_utils/runtime.go
+++ utils/runtime_utils/runtime.go
@@ -2,6 +2,7 @@ package runtime_utils
 
 import (
 	"fmt"
+	"reflect"
 	"runtime"
 )
 
@@ -10,4 +11,8 @@ func ConfigRuntime() {
 	nuCPU := runtime.NumCPU()
 	runtime.GOMAXPROCS(nuCPU)
 	fmt.Printf("Running with %d CPUs\n", nuCPU)
+}
+
+func nameOfFunction(f interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
 }
