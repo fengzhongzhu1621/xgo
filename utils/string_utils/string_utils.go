ab85f2b5b08ed3300bf3383efe6344179408a35f
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-06-28 00:12:55 +0800

    feat: add string join function

--- utils/string_utils/string_utils.go
+++ utils/string_utils/string_utils.go
@@ -1,6 +1,10 @@
 package string_utils
 
-import "strings"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
 
 /**
  * 根据分隔符分割字符串
@@ -11,4 +15,56 @@ func Head(str, sep string) (head string, tail string) {
 		return str, ""
 	}
 	return str[:idx], str[idx+len(sep):]
+}
+
+////////////////////////////////////////////////////////
+// 字符串拼接
+
+// Deprecated: 低效的字符串拼接
+func StringPlus(p []string) string {
+	var s string
+	l := len(p)
+	for i := 0; i < l; i++ {
+		s += p[i]
+	}
+	return s
+}
+
+// Deprecated: 低效的字符串拼接
+func StringFmt(p []interface{}) string {
+	return fmt.Sprint(p...)
+}
+
+// Deprecated: 低效的字符串拼接
+func StringBuffer(p []string) string {
+	var b bytes.Buffer
+	l := len(p)
+	for i := 0; i < l; i++ {
+		b.WriteString(p[i])
+	}
+	return b.String()
+}
+
+func StringJoin(p []string) string {
+	return strings.Join(p, "")
+}
+
+func StringBuilder(p []string) string {
+	var b strings.Builder
+	l := len(p)
+	for i := 0; i < l; i++ {
+		b.WriteString(p[i])
+	}
+	return b.String()
+}
+
+func StringBuilderEx(p []string, cap int) string {
+	var b strings.Builder
+	l := len(p)
+	// 实现分配足够的内容，减少运行时的内存分配
+	b.Grow(cap)
+	for i := 0; i < l; i++ {
+		b.WriteString(p[i])
+	}
+	return b.String()
 }
