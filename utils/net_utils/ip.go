2312e0e0aa2a0a074d684c71f393bfa463f343e6
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-06-27 11:30:52 +0800

    change ip.go

--- utils/net_utils/ip.go
+++ utils/net_utils/ip.go
@@ -1,6 +1,8 @@
 package net_utils
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
 	"strings"
@@ -9,7 +11,7 @@ import (
 /**
  * 通过eth网卡名称获取机器ip
  */
-func GetIpv4ByName(name string) (string, error) {
+func GetIpByEthName(name string) (string, error) {
 	// 返回 interface 结构体对象的列表，包含了全部网卡信息
 	interfaces, err := net.Interfaces()
 	if err != nil {
@@ -26,9 +28,7 @@ func GetIpv4ByName(name string) (string, error) {
 			if addrs, err := v.Addrs(); err == nil {
 				for _, address := range addrs {
 					if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-						if ipnet.IP.To4() != nil {
-							return ipnet.IP.String(), nil
-						}
+						return ipnet.IP.String(), nil
 					}
 				}
 			}
@@ -40,7 +40,7 @@ func GetIpv4ByName(name string) (string, error) {
 /**
  * 通过本机所有的IP
  */
-func GetAllIpv4() []string {
+func GetAllIp() []string {
 	// 返回 interface 结构体对象的列表，包含了全部网卡信息
 	interfaces, err := net.Interfaces()
 	if err != nil {
@@ -58,9 +58,7 @@ func GetAllIpv4() []string {
 		if addrs, err := v.Addrs(); err == nil {
 			for _, address := range addrs {
 				if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-					if ipnet.IP.To4() != nil {
-						ips = append(ips, ipnet.IP.String())
-					}
+					ips = append(ips, ipnet.IP.String())
 				}
 			}
 		}
@@ -80,4 +78,63 @@ func GetLocalConnectionIp(host string, port int) (string, error) {
 	localAddr := conn.LocalAddr().String()
 	idx := strings.LastIndex(localAddr, ":")
 	return localAddr[0:idx], nil
+}
+
+/**
+ * IP从字符串转换为整型
+ */
+func IPtoUint32(ip string) uint32 {
+	// 检查IP地址格式是否有效
+	// 解析为IP地址，并返回该地址。如果s不是合法的IP地址文本表示，ParseIP会返回nil
+	ips := net.ParseIP(ip)
+	if ips == nil {
+		return 0
+	}
+	if len(ips) == net.IPv6len {
+		return binary.BigEndian.Uint32(ips[12:16])
+	} else if len(ips) == net.IPv4len {
+		return binary.BigEndian.Uint32(ips)
+	}
+	return 0
+}
+
+/**
+ * IP从整型转换为字符串
+ */
+func Uint32toIP(ip uint32) string {
+	// 整型转换为字节序列
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, ip)
+	if err != nil {
+		return ""
+	}
+	b := buf.Bytes()
+	return fmt.Sprintf("%d.%d.%d.%d", b[0], b[1], b[2], b[3])
+}
+
+/**
+ * 整型大小端互转
+ *   3    2     1    0
+ *  XX   @@     ##   $$
+ *         ||
+ *         \/
+ *   3    2     1    0
+ *  $$   ##     @@   XX
+ */
+func ConvertEndian(num uint32) uint32 {
+	return ((num >> 24) & 0xff) |
+		((num << 8) & 0xff0000) |
+		((num >> 8) & 0xff00) |
+		((num << 24) & 0xff000000)
+}
+
+/**
+ * 将IP:PORT格式的地址转换IP的整型
+ */
+func AddressToIpUint32(addr string) uint32 {
+	ip, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		return 0
+	}
+	return IPtoUint32(ip)
 }
