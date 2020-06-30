d61e8e7a7d70054395a9616d1e7e58c9b26c6f73
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-06-30 23:54:43 +0800

    feat: add LocateFile

--- utils/file_utils/file.go
+++ utils/file_utils/file.go
@@ -257,3 +257,36 @@ func IsDirType(path string) bool {
 func IsFileType(path string) bool {
 	return !IsDirType(path)
 }
+
+/**
+ * 从指定的目录中查询文件，返回查找到的文件的绝对路径
+ * - 如果文件没有找到，返回err
+ * - 如果查找到多个文件，返回err
+ */
+func LocateFile(filename string, dirs []string) (string, error) {
+	// 在当前目录下查询文件
+	if len(dirs) == 0 || (len(dirs) == 1 && (dirs)[0] == ".") {
+		abs, _ := filepath.Abs(".")
+		return filepath.Join(abs, filename), nil
+	}
+	// 在指定目录下查询文件
+	filepaths := []string{}
+	for _, dir := range dirs {
+		filepath := filepath.Join(dir, filename)
+		if IsFileType(filepath) {
+			filepaths = append(filepaths, filepath)
+		}
+	}
+	// 判断查询到的文件的数量
+	if len(filepaths) == 0 {
+		return "", fmt.Errorf("%s not found in %v", filename, dirs)
+	} else if len(filepaths) > 1 {
+		return "", fmt.Errorf("%s was found in more than one directory: %v", filename, dirs)
+	}
+
+	absPath, err := filepath.Abs(filepaths[0])
+	if err != nil {
+		return "", err
+	}
+	return absPath, nil
+}
--- utils/file_utils/file_test.go
+++ utils/file_utils/file_test.go
@@ -10,23 +10,26 @@ import (
 
 func TestRemoveFileExt(t *testing.T) {
 	tests := []struct {
+		name string
 		filePath string
 		filenameWithoutExt string
 	} {
-		{filePath: "On Unix:", filenameWithoutExt: "On Unix:"},
-		{filePath: "/foo/bar/baz.js", filenameWithoutExt: "baz"},
-		{filePath: "/foo/bar/baz", filenameWithoutExt: "baz"},
-		{filePath: "/foo/bar/baz/", filenameWithoutExt: "baz"},
-		{filePath: "dev.txt", filenameWithoutExt: "dev"},
-		{filePath: "../todo.txt", filenameWithoutExt: "todo"},
-		{filePath: "..", filenameWithoutExt: "."},
-		{filePath: ".", filenameWithoutExt: ""},
-		{filePath: "/", filenameWithoutExt: filepath.FromSlash("/")},
-		{filePath: "", filenameWithoutExt: ""},
+		{name: "1", filePath: "On Unix:", filenameWithoutExt: "On Unix:"},
+		{name: "2", filePath: "/foo/bar/baz.js", filenameWithoutExt: "baz"},
+		{name: "3", filePath: "/foo/bar/baz", filenameWithoutExt: "baz"},
+		{name: "4", filePath: "/foo/bar/baz/", filenameWithoutExt: "baz"},
+		{name: "5", filePath: "dev.txt", filenameWithoutExt: "dev"},
+		{name: "6", filePath: "../todo.txt", filenameWithoutExt: "todo"},
+		{name: "7", filePath: "..", filenameWithoutExt: "."},
+		{name: "8", filePath: ".", filenameWithoutExt: ""},
+		{name: "9", filePath: "/", filenameWithoutExt: filepath.FromSlash("/")},
+		{name: "10", filePath: "", filenameWithoutExt: ""},
 	}
-	for _, test := range tests {
-		actual := RemoveFileExt(test.filePath)
-		assert.Equal(t, actual, test.filenameWithoutExt)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := RemoveFileExt(tt.filePath)
+			assert.Equal(t, actual, tt.filenameWithoutExt)
+		})
 	}
 
 }
@@ -39,3 +42,22 @@ func TestCopy(t *testing.T) {
 	err = os.Remove(dst)
 	assert.NoError(t, err)
 }
+
+func TestLocateFile(t *testing.T) {
+	pwd, _ := os.Getwd()
+	tests := []struct {
+		name     string
+		filename string
+		dirs   []string
+		expect string
+	}{
+		{name: "1", filename: "file.go", dirs: []string{pwd}, expect: ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual, _ := LocateFile(tt.filename, tt.dirs)
+			fmt.Println(actual)
+		})
+	}
+}
+
