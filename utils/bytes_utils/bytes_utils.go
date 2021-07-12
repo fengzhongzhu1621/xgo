298122a1b4d592ab5a2aecae9cf9d8d620b2ff5c
fengzhongzhu1621 <jinyinqiao@gmail.com>
2021-07-12 23:32:17 +0800

    feat: add TrimEOL

--- utils/bytes_utils/bytes_utils.go
+++ utils/bytes_utils/bytes_utils.go
@@ -8,3 +8,19 @@ import "bytes"
 func HasPrefixAndSuffix(s, prefix []byte, suffix []byte) bool {
 	return bytes.HasPrefix(s, prefix) && bytes.HasSuffix(s, suffix)
 }
+
+
+/**
+ * 去掉结尾换行符
+ * trimEOL cuts unixy style \n and windowsy style \r\n suffix from the string
+ */
+func TrimEOL(b []byte) []byte {
+	lns := len(b)
+	if lns > 0 && b[lns-1] == '\n' {
+		lns--
+		if lns > 0 && b[lns-1] == '\r' {
+			lns--
+		}
+	}
+	return b[:lns]
+}
