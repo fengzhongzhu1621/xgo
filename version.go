fb5111cf731b508c5a90fd5556b43ba14c21dbd2
fengzhongzhu1621 <jinyinqiao@gmail.com>
2021-07-12 22:11:51 +0800

    feat: add Version()

--- version.go
+++ version.go
@@ -1,4 +1,19 @@
 package xgo
 
+import (
+	"fmt"
+	"runtime"
+)
+
 // Version is the current package version.
-const Version = "0.0.1"
+const version = "0.0.1"
+
+// This value can be set for releases at build time using:
+//   go {build|run} -ldflags "-X xgo.version 1.2.3 -X xgo.buildInfo timestamp-@githubuser-platform".
+// If unset, Version() shall return "DEVBUILD".
+// var version string = "DEVBUILD"
+var buildInfo = "--"
+
+func Version() string {
+	return fmt.Sprintf("%s (%s %s-%s) %s", version, runtime.Version(), runtime.GOOS, runtime.GOARCH, buildInfo)
+}
