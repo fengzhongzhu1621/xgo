bb65ff5cab1d887861f97d72b5208028aff24d71
fengzhongzhu1621 <jinyinqiao@gmail.com>
2021-07-13 11:52:26 +0800

    test: 添加TrimEOL的单元测试

--- utils/bytes_utils/bytes_utils_test.go
+++ utils/bytes_utils/bytes_utils_test.go
@@ -5,6 +5,17 @@ import (
 	"testing"
 )
 
+var eol_tests = []string{
+	"", "\n", "\r\n", "ok\n", "ok\n",
+	"quite long string for our test\n",
+	"quite long string for our test\r\n",
+}
+
+var eol_answers = []string{
+	"", "", "", "ok", "ok",
+	"quite long string for our test", "quite long string for our test",
+}
+
 func TestHasPrefixAndSuffix(t *testing.T) {
 	s := []byte("ab____cd")
 	prefix := []byte("ab")
@@ -15,4 +26,21 @@ func TestHasPrefixAndSuffix(t *testing.T) {
 	prefix = []byte("abc")
 	actual = HasPrefixAndSuffix(s, prefix, suffix)
 	assert.Equal(t, actual, false, "they should not be equal")
+}
+
+
+func TestTrimEOL(t *testing.T) {
+	for n := 0; n < len(eol_tests); n++ {
+		answ := TrimEOL([]byte(eol_tests[n]))
+		if string(answ) != eol_answers[n] {
+			t.Errorf("Answer '%s' did not match predicted '%s'", answ, eol_answers[n])
+		}
+	}
+}
+
+
+func BenchmarkTrimEOL(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		TrimEOL([]byte(eol_tests[n%len(eol_tests)]))
+	}
 }
