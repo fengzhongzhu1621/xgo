c675c0fc83b987ad6bd29e9c656ef4ae2d2ed395
fengzhongzhu1621 <jinyinqiao@gmail.com>
2021-07-13 11:52:39 +0800

    feat: add pipe

---
+++ pipe/endpoint.go
@@ -0,0 +1,34 @@
+package pipe
+
+type Endpoint interface {
+	StartReading()			// 启动协程读取数据，写入到管道中
+	Terminate()
+	Output() chan []byte	// 返回管道
+	Send([]byte) bool
+}
+
+/**
+ * 双工管道传递数据
+ */
+func PipeEndpoints(e1, e2 Endpoint) {
+	// 启动协程读取数据，写入到管道中
+	e1.StartReading()
+	e2.StartReading()
+
+	defer e1.Terminate()
+	defer e2.Terminate()
+
+	// 双向管道数据发送
+	for {
+		select {
+		case msgOne, ok := <-e1.Output():
+			if !ok || !e2.Send(msgOne) {
+				return
+			}
+		case msgTwo, ok := <-e2.Output():
+			if !ok || !e1.Send(msgTwo) {
+				return
+			}
+		}
+	}
+}
