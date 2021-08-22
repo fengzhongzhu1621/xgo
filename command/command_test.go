dc17fc4215f633c608b4fcf571df52d98df59fc4
fengzhongzhu1621 <jinyinqiao@gmail.com>
2021-08-22 15:09:59 +0800

    test: 修复单元测试

--- command/command_test.go
+++ command/command_test.go
@@ -1,13 +1,17 @@
 package command
 
 import (
-	"github.com/stretchr/testify/assert"
+	"runtime"
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRunBashCommand(t *testing.T) {
-	err, out, errout := RunBashCommand("echo 1")
-	assert.Equal(t, nil, err)
-	assert.Equal(t, "1\n", out)
-	assert.Equal(t, "", errout)
+	sysType := runtime.GOOS
+	if sysType != "windows" {
+		err, out, errout := RunBashCommand("echo 1")
+		assert.Equal(t, nil, err)
+		assert.Equal(t, "1\n", out)
+		assert.Equal(t, "", errout)
+	}
 }
