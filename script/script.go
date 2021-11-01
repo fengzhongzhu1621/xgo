4ff87cb31ce5979adbbe4b477ea2ccba431ceafe
fengzhongzhu1621 <jinyinqiao@gmail.com>
2021-11-01 19:31:52 +0800

    feat: add script package

---
+++ script/script.go
@@ -0,0 +1,25 @@
+package script
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"io"
+)
+
+type Script struct {
+	src  string
+	hash string
+}
+
+func NewScript(src string) *Script {
+	h := sha1.New()
+	_, _ = io.WriteString(h, src)
+	return &Script{
+		src:  src,
+		hash: hex.EncodeToString(h.Sum(nil)),
+	}
+}
+
+func (s *Script) Hash() string {
+	return s.hash
+}
