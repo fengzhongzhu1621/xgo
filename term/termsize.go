67f921b4518d965c82b420291b526f1e3487f423
fengzhongzhu1621 <jinyinqiao@gmail.com>
2021-11-08 19:15:20 +0800

    feat: add term package

---
+++ term/termsize.go
@@ -0,0 +1,16 @@
+// +build !windows,!plan9,!appengine,!wasm
+
+package term
+
+import (
+	"golang.org/x/sys/unix"
+)
+
+// 获得终端的列数.
+func getTerminalColumns() int {
+	ws, err := unix.IoctlGetWinsize(0, unix.TIOCGWINSZ)
+	if err != nil {
+		return 80
+	}
+	return int(ws.Col)
+}
