a12a7658deb43493f2a90cab519e69221fdc3e6a
fengzhongzhu1621 <jinyinqiao@gmail.com>
2022-07-07 00:37:52 +0800

    feat: add buffer pool

---
+++ pool/buffer_pool.go
@@ -0,0 +1,46 @@
+package pool
+
+import (
+	"bytes"
+	"sync"
+)
+
+var (
+	bufferPool BufferPool
+)
+
+type BufferPool interface {
+	Put(*bytes.Buffer)
+	Get() *bytes.Buffer
+}
+
+type defaultPool struct {
+	pool *sync.Pool
+}
+
+// Put 把不用的对象放回或者提前放到 sync.Pool 中
+func (p *defaultPool) Put(buf *bytes.Buffer) {
+	p.pool.Put(buf)
+}
+
+func (p *defaultPool) Get() *bytes.Buffer {
+	return p.pool.Get().(*bytes.Buffer)
+}
+
+// SetBufferPool allows to replace the default logrus buffer pool
+// to better meets the specific needs of an application.
+func SetBufferPool(bp BufferPool) {
+	bufferPool = bp
+}
+
+// bytes.buffer是一个缓冲byte类型的缓冲器，使用 sync.Pool 减少 bytes.Buffer 创建的成本
+// 保存和复用临时对象，减少内存分配，降低 GC 压力。
+func init() {
+	SetBufferPool(&defaultPool{
+		pool: &sync.Pool{
+			New: func() interface{} {
+				return new(bytes.Buffer)
+			},
+		},
+	})
+}
