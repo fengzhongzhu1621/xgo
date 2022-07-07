57ad97fe08ff8de826abaad8a63416cf9e3b1fe2
fengzhongzhu1621 <jinyinqiao@gmail.com>
2022-07-08 00:06:10 +0800

    test: add level_test.go

--- log/level_test.go
+++ log/level_test.go
@@ -1,1 +1,63 @@
 package log
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLevelJsonEncoding(t *testing.T) {
+	type X struct {
+		level Level
+	}
+
+	var x X
+	x.level = WarnLevel
+	var buf bytes.Buffer
+	// 将对象转化为json字节数组
+	enc := json.NewEncoder(&buf)
+	require.NoError(t, enc.Encode(x))
+	// 将字节数组转化为对象
+	dec := json.NewDecoder(&buf)
+	var y X
+	require.NoError(t, dec.Decode(&y))
+}
+
+func TestLevelUnmarshalText(t *testing.T) {
+	var u Level
+	for _, level := range AllLevels {
+		t.Run(level.String(), func(t *testing.T) {
+			require.NoError(t, u.UnmarshalText([]byte(level.String())))
+			require.Equal(t, level, u)
+		})
+	}
+	t.Run("invalid", func(t *testing.T) {
+		require.Error(t, u.UnmarshalText([]byte("invalid")))
+	})
+}
+
+func TestLevelMarshalText(t *testing.T) {
+	levelStrings := []string{
+		"panic",
+		"fatal",
+		"error",
+		"warning",
+		"info",
+		"debug",
+		"trace",
+	}
+	for idx, val := range AllLevels {
+		level := val
+		t.Run(level.String(), func(t *testing.T) {
+			var cmp Level
+			b, err := level.MarshalText()
+			require.NoError(t, err)
+			require.Equal(t, levelStrings[idx], string(b))
+			err = cmp.UnmarshalText(b)
+			require.NoError(t, err)
+			require.Equal(t, level, cmp)
+		})
+	}
+}
