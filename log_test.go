7a6018b953e0721a77bacfdced724535a86df6f1
fengzhongzhu1621 <jinyinqiao@gmail.com>
2022-07-22 22:17:37 +0800

    feat: add log_test.go

---
+++ log_test.go
@@ -0,0 +1,39 @@
+package xgo
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"testing"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func TestLog(t *testing.T) {
+	u := User{
+		Name: "dj",
+		Age:  18,
+	}
+	log.SetPrefix("Login: ")
+	log.SetFlags(log.Lshortfile | log.Ldate | log.Lmicroseconds)
+	log.Printf("%s login, age:%d", u.Name, u.Age)
+
+	buf := &bytes.Buffer{}
+	logger := log.New(buf, "", log.Lshortfile|log.LstdFlags)
+	logger.Printf("%s login, age:%d", u.Name, u.Age)
+	fmt.Print(buf.String())
+
+	writer1 := &bytes.Buffer{}
+	writer2 := os.Stdout
+	writer3, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatalf("create file log.txt failed: %v", err)
+	}
+	logger = log.New(io.MultiWriter(writer1, writer2, writer3), "", log.Lshortfile|log.LstdFlags)
+	logger.Printf("%s login, age:%d", u.Name, u.Age)
+}
