2d7e2668bf3e12d69b480cb64317aaa28a7e0f62
fengzhongzhu1621 <jinyinqiao@gmail.com>
2022-08-14 22:30:40 +0800

    feat: add sync

---
+++ utils/sync/waitgroup.go
@@ -0,0 +1,24 @@
+package sync
+
+import (
+	"sync"
+	"time"
+)
+
+// WaitGroupTimeout adds timeout feature for sync.WaitGroup.Wait().
+// It returns true, when timeouted.
+func WaitGroupTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
+	wgClosed := make(chan struct{}, 1)
+	go func() {
+		// 等待协程执行完成
+		wg.Wait()
+		wgClosed <- struct{}{}
+	}()
+
+	select {
+	case <-wgClosed:
+		return false
+	case <-time.After(timeout):
+		return true
+	}
+}
