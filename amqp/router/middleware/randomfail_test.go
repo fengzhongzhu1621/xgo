0e4d53d1b1470495c96fd099ebd9f8c2fb159580
fengzhongzhu1621 <jinyinqiao@gmail.com>
2022-09-14 23:33:26 +0800

    feat: add randomfail middleware

---
+++ amqp/router/middleware/randomfail_test.go
@@ -0,0 +1,29 @@
+package middleware_test
+
+import (
+	"testing"
+
+	"xgo/amqp/message"
+	"xgo/amqp/router/middleware"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRandomFail(t *testing.T) {
+	h := middleware.RandomFail(1)(func(msg *message.Message) (messages []*message.Message, e error) {
+		return nil, nil
+	})
+
+	_, err := h(message.NewMessage("1", nil))
+	assert.Error(t, err)
+}
+
+func TestRandomPanic(t *testing.T) {
+	h := middleware.RandomPanic(1)(func(msg *message.Message) (messages []*message.Message, e error) {
+		return nil, nil
+	})
+
+	assert.Panics(t, func() {
+		_, _ = h(message.NewMessage("1", nil))
+	})
+}
