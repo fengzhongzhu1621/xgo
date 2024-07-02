08c4c5593a0f2d6717f273802990486e98624d5f
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-07-03 00:32:05 +0800

    feat: add ParseTemplate

---
+++ tpl/res.go
@@ -0,0 +1,27 @@
+package tpl
+
+import "html/template"
+
+var (
+	tmpl      *template.Template
+	templates = map[string]string{
+		"index":       "res/index.tmpl.html",
+		"ipa-install": "res/ipa-install.tmpl.html",
+	}
+)
+
+// 创建一个名为 name 的模板对象，解析模板内容 content
+func ParseTemplate(name string, content string) {
+	if tmpl == nil {
+		// 创建一个新的模板对象
+		tmpl = template.New(name)
+	}
+	var t *template.Template
+	if tmpl.Name() == name {
+		t = tmpl
+	} else {
+		t = tmpl.New(name)
+	}
+	// 自定义模板分隔符，使用 Parse() 解析模板内容，使用 Must 函数处理潜在的错误
+	template.Must(t.New(name).Delims("[[", "]]").Parse(content))
+}
