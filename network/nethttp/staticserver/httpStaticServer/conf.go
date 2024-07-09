13b0637c3b686ca153a0795497fcf44b2f5115d6
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-07-10 00:40:54 +0800

    func (s *HTTPStaticServer) readAccessConf

--- network/nethttp/staticserver/httpStaticServer/conf.go
+++ network/nethttp/staticserver/httpStaticServer/conf.go
@@ -1,7 +1,14 @@
 package httpStaticServer
 
 import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/fengzhongzhu1621/xgo/crypto/zipfile"
+	"github.com/fengzhongzhu1621/xgo/file"
 	"github.com/fengzhongzhu1621/xgo/network/nethttp/staticServer"
+	"gopkg.in/yaml.v2"
 )
 
 // 构造默认配置对象
@@ -10,4 +17,41 @@ func (s *HTTPStaticServer) defaultAccessConf() staticServer.AccessConf {
 		Upload: s.Upload,
 		Delete: s.Delete,
 	}
+}
+
+// 读取静态文件关联的配置文件，转换为结构体
+func (s *HTTPStaticServer) readAccessConf(realPath string) (ac staticServer.AccessConf) {
+	// 获得静态文件的相对路径，先判断静态文件是否在根目录
+	relativePath, err := filepath.Rel(s.Root, realPath)
+	if err != nil || relativePath == "." || relativePath == "" { // actually relativePath is always "." if root == realPath
+		ac = s.defaultAccessConf()
+		// 如果静态文件在根目录，则读取根目录的配置文件
+		realPath = s.Root
+	} else {
+		// 如果静态文件不在根目录，
+		parentPath := filepath.Dir(realPath)
+		// 向上遍历获取祖先目录下的配置文件，先将根据目录下的配置转换为结构体，然后在将子目录下的配置文件转换为结构体并覆盖父配置
+		ac = s.readAccessConf(parentPath)
+	}
+	if file.IsFile(realPath) {
+		realPath = filepath.Dir(realPath)
+	}
+
+	// 获得配置文件的路径
+	cfgFile := filepath.Join(realPath, zipfile.YAMLCONF)
+	// 读取配置文件
+	data, err :=  os.ReadFile(cfgFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return ac
+		}
+		log.Printf("Err read .ghs.yml: %v", err)
+	}
+	// 将yaml文件转换为结构体对象
+	err = yaml.Unmarshal(data, &ac)
+	if err != nil {
+		log.Printf("Err format .ghs.yml: %v", err)
+	}
+
+	return ac
 }
