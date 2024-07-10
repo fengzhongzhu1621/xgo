7188f464dbdfeb79b72c3ca21a3717769a809320
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-07-10 23:39:46 +0800

    feat: add (s *HTTPStaticServer) findIndex

--- network/nethttp/staticserver/httpStaticServer/index.go
+++ network/nethttp/staticserver/httpStaticServer/index.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 )
 
 // 生成所有静态文件的索引配置
@@ -32,4 +33,36 @@ func (s *HTTPStaticServer) makeIndex() error {
 	
 	s.indexes = indexes
 	return err
+}
+
+
+// 查找索引，根据相对路径查找
+func (s *HTTPStaticServer) findIndex(text string) []IndexFileItem {
+	ret := make([]IndexFileItem, 0)
+	// 遍历索引配置
+	for _, item := range s.indexes {
+		ok := true
+		// search algorithm, space for AND
+		// 将一个字符串按照空白字符（如空格、制表符、换行符等）进行分割，并返回一个包含分割后的子字符串的切片（slice）
+		for _, keyword := range strings.Fields(text) {
+			needContains := true
+			// 去掉开头的 -，排除类似于 exclude
+			if strings.HasPrefix(keyword, "-") {
+				needContains = false
+				keyword = keyword[1:]
+			}
+			if keyword == "" {
+				continue
+			}
+			// 检查一个字符串是否包含另一个子字符串。
+			ok = (needContains == strings.Contains(strings.ToLower(item.Path), strings.ToLower(keyword)))
+			if !ok {
+				break
+			}
+		}
+		if ok {
+			ret = append(ret, item)
+		}
+	}
+	return ret
 }
