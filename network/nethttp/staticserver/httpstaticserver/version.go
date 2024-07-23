b28914a49e173cbfc5cc596b5c85f887ef0fdd43
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-07-23 23:18:28 +0800

    feat: add versionMessage method

---
+++ network/nethttp/staticserver/httpstaticserver/version.go
@@ -0,0 +1,29 @@
+package httpstaticserver
+
+import (
+	"bytes"
+	"html/template"
+	"runtime"
+)
+
+func versionMessage() string {
+	// 创建字符串模板
+	t := template.Must(template.New("version").Parse(`GoHTTPServer
+  Version:        {{.Version}}
+  Go version:     {{.GoVersion}}
+  OS/Arch:        {{.OSArch}}
+  Git commit:     {{.GitCommit}}
+  Built:          {{.Built}}
+  Site:           {{.Site}}`))
+	// 渲染模板
+	buf := bytes.NewBuffer(nil)
+	t.Execute(buf, map[string]interface{}{
+		"Version":   VERSION,
+		"GoVersion": runtime.Version(),
+		"OSArch":    runtime.GOOS + "/" + runtime.GOARCH,
+		"GitCommit": GITCOMMIT,
+		"Built":     BUILDTIME,
+		"Site":      SITE,
+	})
+	return buf.String()
+}
