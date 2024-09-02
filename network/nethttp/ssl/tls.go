2dc0fd51da8a6eeed01d40167cd8c336591bcaf9
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-09-03 00:06:21 +0800

    feat: add ssl methods

--- network/nethttp/ssl/tls.go
+++ network/nethttp/ssl/tls.go
@@ -9,3 +9,89 @@ func ClientTslConfNoVerity() *tls.Config {
 		InsecureSkipVerify: true,
 	}
 }
+
+// ClientTslConfVerityServer 用于配置 TLS 客户端的 SSL/TLS 设置，以便验证服务器的证书。
+func ClientTslConfVerityServer(caFile string) (*tls.Config, error) {
+	// 读取 CA 证书文件，返回证书池
+	certPool, err := LoadX509Cert(caFile)
+	if err != nil {
+		return nil, err
+	}
+
+	// 配置 TLS 客户端
+	tlsConfig := &tls.Config{
+		RootCAs: certPool,
+	}
+
+	return tlsConfig, nil
+}
+
+// ClientTslConfVerity 用于创建一个 TLS 客户端的 SSL/TLS 配置，包括加载 CA 证书、客户端证书和私钥
+func ClientTslConfVerity(caFile, certFile, keyFile, passwd string) (*tls.Config, error) {
+	// 读取 CA 证书文件
+	certPool, err := LoadX509Cert(caFile)
+	if err != nil {
+		return nil, err
+	}
+	// 从指定的客户端证书和私钥文件中加载 TLS 证书，并在需要时解密私钥，返回一个 *tls.Certificate 对象
+	cert, err := LoadX509Certificates(certFile, keyFile, passwd)
+	if err != nil {
+		return nil, err
+	}
+
+	// 配置 TLS 客户端
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true, // 跳过服务器证书的验证
+		RootCAs:            certPool, // CA 证书池，用于验证服务器证书
+		Certificates:       []tls.Certificate{*cert}, // 客户端证书和私钥
+	}
+
+	return tlsConfig, nil
+}
+
+// ServerTslConf 服务端Tsl配置
+func ServerTslConf(caFile, certFile, keyFile, passwd string) (*tls.Config, error) {
+	if "" == caFile {
+		return ServerTslConfVerity(certFile, keyFile, passwd)
+	}
+
+	return ServerTslConfVerityClient(caFile, certFile, keyFile, passwd)
+}
+
+// ServerTslConfVerity 创建一个 TLS 服务器的 SSL/TLS 配置，包括加载服务器证书和私钥。
+func ServerTslConfVerity(certFile, keyFile, passwd string) (*tls.Config, error) {
+	cert, err := LoadX509Certificates(certFile, keyFile, passwd)
+	if err != nil {
+		return nil, err
+	}
+
+	conf := &tls.Config{
+		Certificates: []tls.Certificate{*cert}, // 服务器证书和私钥
+	}
+
+	return conf, nil
+}
+
+// ServerTslConfVerityClient 创建一个 TLS 服务器的 SSL/TLS 配置，包括加载 CA 证书、服务器证书和私钥，并要求客户端提供并验证其证书。
+// 这有助于确保只有拥有有效客户端证书的用户才能访问服务器。
+func ServerTslConfVerityClient(caFile, certFile, keyFile, passwd string) (*tls.Config, error) {
+	// 读取 CA 证书文件
+	caPool, err := LoadX509Cert(caFile)
+	if err != nil {
+		return nil, err
+	}
+
+	// 从指定的证书和私钥文件中加载 TLS 证书，并在需要时解密私钥
+	cert, err := LoadX509Certificates(certFile, keyFile, passwd)
+	if err != nil {
+		return nil, err
+	}
+
+	conf := &tls.Config{
+		ClientCAs:    caPool, // CA 证书池，用于验证客户端证书
+		Certificates: []tls.Certificate{*cert}, // 服务器证书和私钥
+		ClientAuth:   tls.RequireAndVerifyClientCert, // 要求客户端提供证书并验证其有效性
+	}
+
+	return conf, nil
+}
--- network/nethttp/ssl/x509.go
+++ network/nethttp/ssl/x509.go
@@ -1,7 +1,9 @@
 package ssl
 
 import (
+	"crypto/tls"
 	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"os"
 )
@@ -20,5 +22,49 @@ func LoadX509Cert(caFile string) (*x509.CertPool, error) {
 		return nil, fmt.Errorf("append ca cert failed")
 	}
 
+	// 返回证书池
 	return certPool, nil
+}
+
+
+// LoadCertificates 从指定的客户端证书和私钥文件中加载 TLS 证书，并在需要时解密私钥，返回一个 *tls.Certificate 对象
+func LoadX509Certificates(certFile, keyFile, passwd string) (*tls.Certificate, error) {
+	// 读取私钥文件
+	privateKey, err := os.ReadFile(keyFile)
+	if err != nil {
+		return nil, err
+	}
+
+	// 如果提供了密码，尝试解密私钥
+	if "" != passwd {
+		// 首先使用 pem.Decode() 函数解码 PEM 格式的私钥
+		privatePem, _ := pem.Decode(privateKey)
+		if privatePem == nil {
+			return nil, fmt.Errorf("decode private key failed")
+		}
+		// 如果解码失败，返回错误信息。然后使用 x509.DecryptPEMBlock() 函数解密私钥
+		privateDecryptPem, err := x509.DecryptPEMBlock(privatePem, []byte(passwd))
+		if err != nil {
+			return nil, err
+		}
+		// 将解密后的私钥重新编码为 PEM 格式
+		privateKey = pem.EncodeToMemory(&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: privateDecryptPem,
+		})
+	}
+
+	// 读取自定义的 CA 证书文件
+	certData, err := os.ReadFile(certFile)
+	if err != nil {
+		return nil, err
+	}
+
+	// 将证书数据和私钥数据组合成一个 tls.Certificate 对象
+	tlsCert, err := tls.X509KeyPair(certData, privateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tlsCert, nil
 }
