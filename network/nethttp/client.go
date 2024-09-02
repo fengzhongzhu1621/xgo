95397f38a69f90c11ca73b74fb74fef81cabd555
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-09-03 00:45:34 +0800

    feat: add HttpClient methods

--- network/nethttp/client.go
+++ network/nethttp/client.go
@@ -1,13 +1,22 @@
 package nethttp
 
 import (
+	"bytes"
 	"crypto/tls"
+	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"time"
 
 	"github.com/fengzhongzhu1621/xgo/network/nethttp/ssl"
 )
+type HttpRespone struct {
+	Reply      []byte // 响应内容
+	StatusCode int
+	Status     string
+	Header     http.Header
+}
 
 type HttpClient struct {
 	caFile   string
@@ -110,4 +119,150 @@ func (client *HttpClient) SetBatchHeader(headerSet []*HeaderSet) {
 	for _, header := range headerSet {
 		client.header[header.Key] = header.Value
 	}
+}
+
+// RequestEx do http request, old version
+func (client *HttpClient) RequestEx(url, method string, header http.Header, data []byte) (*HttpRespone, error) {
+	var req *http.Request
+	var errReq error
+	httpRsp := &HttpRespone{
+		Reply:      nil,
+		StatusCode: http.StatusInternalServerError,
+		Status:     "Internal Server Error",
+	}
+
+	// 用于创建一个新的 HTTP 请求。该函数接受三个参数：请求方法（如 "GET"、"POST" 等），请求的 URL 和可选的请求体
+	// （对于某些请求方法，如 "GET"，请求体通常为空）。
+	if data != nil {
+		req, errReq = http.NewRequest(method, url, bytes.NewReader(data))
+	} else {
+		req, errReq = http.NewRequest(method, url, nil)
+	}
+
+	if errReq != nil {
+		return httpRsp, errReq
+	}
+
+	req.Close = true
+
+	// 设置 header
+	if header != nil {
+		req.Header = header
+	}
+	for key, value := range client.header {
+		if req.Header.Get(key) != "" {
+			continue
+		}
+		req.Header.Set(key, value)
+	}
+
+	// 发送请求
+	rsp, err := client.httpCli.Do(req)
+	if err != nil {
+		return httpRsp, err
+	}
+
+	defer rsp.Body.Close()
+
+	httpRsp.Status = rsp.Status
+	httpRsp.StatusCode = rsp.StatusCode
+	httpRsp.Header = rsp.Header
+
+	// 读取响应内容
+	rpy, err := io.ReadAll(rsp.Body)
+	if err != nil {
+		return httpRsp, err
+	}
+	httpRsp.Reply = rpy
+
+	return httpRsp, nil
+}
+
+func (client *HttpClient) RequestStream(url, method string, header http.Header, data []byte) (io.ReadCloser, error) {
+	var req *http.Request
+	var errReq error
+
+	// 用于创建一个新的 HTTP 请求。该函数接受三个参数：请求方法（如 "GET"、"POST" 等），请求的 URL 和可选的请求体
+	// （对于某些请求方法，如 "GET"，请求体通常为空）。
+	if data != nil {
+		req, errReq = http.NewRequest(method, url, bytes.NewReader(data))
+	} else {
+		req, errReq = http.NewRequest(method, url, nil)
+	}
+
+	if errReq != nil {
+		return nil, errReq
+	}
+
+	req.Close = true
+
+	// 设置 header
+	if header != nil {
+		req.Header = header
+	}
+	for key, value := range client.header {
+		if req.Header.Get(key) != "" {
+			continue
+		}
+		req.Header.Set(key, value)
+	}
+
+	// 发送请求
+	rsp, err := client.httpCli.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	switch {
+	case (rsp.StatusCode >= 200) && (rsp.StatusCode < 300):
+		return rsp.Body, nil
+	default:
+		defer rsp.Body.Close()
+		return nil, fmt.Errorf("get stream failed, resp code %d", rsp.StatusCode)
+	}
+}
+
+func (client *HttpClient) GET(url string, header http.Header, data []byte) ([]byte, error) {
+	return client.Request(url, "GET", header, data)
+}
+
+func (client *HttpClient) POST(url string, header http.Header, data []byte) ([]byte, error) {
+	return client.Request(url, "POST", header, data)
+}
+
+func (client *HttpClient) DELETE(url string, header http.Header, data []byte) ([]byte, error) {
+	return client.Request(url, "DELETE", header, data)
+}
+
+func (client *HttpClient) PUT(url string, header http.Header, data []byte) ([]byte, error) {
+	return client.Request(url, "PUT", header, data)
+}
+
+func (client *HttpClient) PATCH(url string, header http.Header, data []byte) ([]byte, error) {
+	return client.Request(url, "PATCH", header, data)
+}
+
+func (client *HttpClient) Get(url string, header http.Header, data []byte) (*HttpRespone, error) {
+	return client.RequestEx(url, "GET", header, data)
+}
+
+func (client *HttpClient) Post(url string, header http.Header, data []byte) (*HttpRespone, error) {
+	return client.RequestEx(url, "POST", header, data)
+}
+
+func (client *HttpClient) Delete(url string, header http.Header, data []byte) (*HttpRespone, error) {
+	return client.RequestEx(url, "DELETE", header, data)
+}
+
+func (client *HttpClient) Put(url string, header http.Header, data []byte) (*HttpRespone, error) {
+	return client.RequestEx(url, "PUT", header, data)
+}
+
+func (client *HttpClient) Patch(url string, header http.Header, data []byte) (*HttpRespone, error) {
+	return client.RequestEx(url, "PATCH", header, data)
+}
+
+func (client *HttpClient) Request(url, method string, header http.Header, data []byte) ([]byte, error) {
+	rsp, err := client.RequestEx(url, method, header, data)
+	return rsp.Reply, err
 }
