e8cc503ddeae84e2b5b5b90321c5882c4fcbb1a1
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-09-07 23:41:47 +0800

    feat: add cache key struct

--- cache/multitag.go
+++ cache/multitag.go
@@ -6,12 +6,14 @@ type MultiTag struct {
 	cache map[string][]string
 }
 
+// NewMultiTag
 func NewMultiTag(v string) MultiTag {
 	return MultiTag{
 		value: v,
 	}
 }
 
+// Parse 初始化缓存
 func (x *MultiTag) Parse() error {
 	vals, err := x.Scan()
 	x.cache = vals
@@ -19,6 +21,7 @@ func (x *MultiTag) Parse() error {
 	return err
 }
 
+// cached 获得缓存对象，没有则初始化缓存对象
 func (x *MultiTag) cached() map[string][]string {
 	if x.cache == nil {
 		cache, _ := x.Scan()
@@ -33,9 +36,12 @@ func (x *MultiTag) cached() map[string][]string {
 	return x.cache
 }
 
+// Get 从缓存中获取值
 func (x *MultiTag) Get(key string) string {
+	// 获得缓存对象
 	c := x.cached()
 
+	// 从缓存中获取值，取最新的一条记录
 	if v, ok := c[key]; ok {
 		return v[len(v)-1]
 	}
