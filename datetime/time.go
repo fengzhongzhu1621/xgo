125ff9a6d57045ffef7133dbdaeab9f76b212e0e
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-09-11 00:00:10 +0800

    feat: add TodayStartTimestamp func

--- datetime/time.go
+++ datetime/time.go
@@ -47,7 +47,15 @@ func Sleep(ctx context.Context, dur time.Duration) error {
 	}
 }
 
-// SecondToTime 把事件戳转换为time.Time格式.
+// SecondToTime 把时间戳转换为time.Time格式.
 func SecondToTime(second int64) time.Time {
 	return time.Unix(second, 0)
+}
+
+// TodayStartTimestamp 返回今天开始时间的 Unix 时间戳（自 1970 年 1月1日 00:00:00 UTC 起经过的秒数）。
+func TodayStartTimestamp() int64 {
+	now := time.Now()
+	year, month, day := now.Date()
+	start := time.Date(year, month, day, 0, 0, 0, 0, now.Location())
+	return start.Unix()
 }
