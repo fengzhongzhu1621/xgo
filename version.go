e25fc1ae17853a27572e3a7d18df65266ed2eaa0
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-09-12 19:32:35 +0800

    feat: add VersionList type

--- version.go
+++ version.go
@@ -15,3 +15,25 @@ func GetVersion() string {
 	version := fmt.Sprintf("Version  :%s\n", version)
 	return version
 }
+
+// /////////////////////////////////////////////////////////////////////////
+type VersionList [][3]int
+
+func (m VersionList) Len() int {
+	return len(m)
+}
+
+// Less 版本号比较
+func (m VersionList) Less(i, j int) bool {
+	for x := range m[i] {
+		// 版本号比较
+		if m[i][x] == m[j][x] {
+			continue
+		}
+		return m[i][x] < m[j][x]
+	}
+	return false
+}
+
+// Swap 版本号交换
+func (m VersionList) Swap(i, j int) { m[i], m[j] = m[j], m[i] }
