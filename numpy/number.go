ab7a23fe1e3d3cfb75fe1ae227453757bc674279
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-09-12 20:49:19 +0800

    feat: add Chunk func for int64 array

--- numpy/number.go
+++ numpy/number.go
@@ -16,3 +16,21 @@ func RemoveDuplicatesInt64(numbers []int64) []int64 {
 	}
 	return result
 }
+
+// Chunk 将整型数组按指定长度分组
+func Chunk(slice []int64, chunkSize int) [][]int64 {
+	var (
+		chunks [][]int64
+	)
+	for i := 0; i < len(slice); i += chunkSize {
+		end := i + chunkSize
+
+		if end > len(slice) {
+			end = len(slice)
+		}
+
+		chunks = append(chunks, slice[i:end])
+	}
+
+	return chunks
+}
