73bc85efa3d19e93d9f237dc9a8e2c3059a0a646
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-10-05 11:55:32 +0800

    feat: add ToString

--- tests/print.go
+++ tests/print.go
@@ -6,7 +6,15 @@ import (
 	"github.com/dablelv/cyan/encoding"
 )
 
+// PrintStruct 打印结构体
 func PrintStruct(obj interface{}) {
 	s, _ := encoding.ToIndentJSON(obj)
 	fmt.Printf("%v\n", s)
+}
+
+// ToString 将结构体转换为字符串表示
+func ToString(obj interface{}) string {
+	s, _ := encoding.ToIndentJSON(obj)
+	result := fmt.Sprintf("%v", s)
+	return result
 }
