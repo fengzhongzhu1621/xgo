df29bf23170abb356ebb42be4cf8aa0c9079247e
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-10-19 00:13:16 +0800

    feat: add Probability struct

--- crypto/randutils/rand.go
+++ crypto/randutils/rand.go
@@ -3,6 +3,7 @@ package randutils
 import (
 	crand "crypto/rand"
 	"fmt"
+	"math/big"
 	"math/rand"
 	"sync"
 	"time"
@@ -11,6 +12,11 @@ import (
 var (
 	letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 	sources     = []byte("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+)
+
+var (
+	character = []byte("abcdefghijklmnopqrstuvwxyz0123456789")
+	chLen     = len(character)
 )
 
 var tmpMu sync.Mutex
@@ -41,7 +47,7 @@ type source struct {
 	mu  sync.Mutex
 }
 
-// 支持多线程安全.
+// Int63 支持多线程安全.
 func (s *source) Int63() int64 {
 	s.mu.Lock()
 	n := s.src.Int63()
@@ -49,7 +55,7 @@ func (s *source) Int63() int64 {
 	return n
 }
 
-// 支持多线程安全.
+// Seed 支持多线程安全.
 func (s *source) Seed(seed int64) {
 	s.mu.Lock()
 	s.src.Seed(seed)
@@ -70,6 +76,7 @@ func RandomString(length int) string {
 	return string(b)
 }
 
+// RandString2 生成一个指定长度的随机字符串
 func RandString2(length int64) string {
 	var (
 		result []byte
@@ -83,6 +90,7 @@ func RandString2(length int64) string {
 	return string(result)
 }
 
+// RandAuthToken 随机生成 token
 func RandAuthToken() string {
 	buf := make([]byte, 32)
 	_, err := crand.Read(buf)
@@ -91,4 +99,47 @@ func RandAuthToken() string {
 	}
 
 	return fmt.Sprintf("%x", buf)
+}
+
+// Random 随机数据
+func Random(size int) []byte {
+	buf := make([]byte, size)
+
+	if _, err := crand.Read(buf); err != nil {
+		rand.Seed(time.Now().UnixNano())
+		rand.Read(buf)
+	}
+	return buf
+}
+
+// RandomInt 生成一个指定范围的随机整数
+func RandomInt(min, max int) int {
+	// crand.Int 是一个生成随机数的函数，它使用加密安全的随机数生成器（CSPRNG）
+	// crand.Reader 是一个全局、共享的加密安全随机数生成器
+	// big.NewInt(int64(max-min)) 创建一个大的整数，表示生成随机数的范围
+	random, err := crand.Int(crand.Reader, big.NewInt(int64(max-min)))
+	if err != nil {
+		rand.Seed(time.Now().UnixNano())
+		// 生成一个指定范围内的随机整数
+		return rand.Intn(max-min) + min
+	}
+
+	return int(random.Int64()) + min
+}
+
+// RandomString 随机字符串
+func RandomString3(size int) string {
+	buf := make([]byte, size, size)
+	max := big.NewInt(int64(chLen))
+	for i := 0; i < size; i++ {
+		random, err := crand.Int(crand.Reader, max)
+		if err != nil {
+			rand.Seed(time.Now().UnixNano())
+			buf[i] = character[rand.Intn(chLen)]
+			continue
+		}
+		buf[i] = character[random.Int64()]
+	}
+
+	return string(buf)
 }
