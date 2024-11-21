1b47c10118d05c1def5da578e893f24012a92f9a
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-11-21 23:45:43 +0800

    feat: add cors middleware

---
+++ ginx/middleware/cors.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+// CORS 处理跨域请求
+func CORS(allowedOrigins []string) gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     allowedOrigins,
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
+}
