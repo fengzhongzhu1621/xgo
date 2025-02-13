5286c9f65fb5f71f7e25ca76f7f77566c1afa7df
fengzhongzhu1621 <jinyinqiao@gmail.com>
2025-02-13 23:54:39 +0800

    feat: add beginswith operator

--- validator/str.go
+++ validator/str.go
@@ -1,5 +1,11 @@
 package validator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
 // IsLower 判断字符串是否包含小写字母.
 func IsLower(s string) bool {
 	for i := 0; i < len(s); i++ {
@@ -9,4 +15,25 @@ func IsLower(s string) bool {
 		}
 	}
 	return true
+}
+
+// ValidateStringType validate if the value is a string type
+func ValidateStringType(value interface{}) error {
+	if reflect.TypeOf(value).Kind() != reflect.String {
+		return fmt.Errorf("value(%+v) is not of string type", value)
+	}
+	return nil
+}
+
+// ValidateNotEmptyStringType validate if the value is a not empty string type
+func ValidateNotEmptyStringType(value interface{}) error {
+	strVal, ok := value.(string)
+	if !ok {
+		return fmt.Errorf("value(%+v) is not of string type", value)
+	}
+
+	if len(strVal) == 0 {
+		return errors.New("value is empty")
+	}
+	return nil
 }
