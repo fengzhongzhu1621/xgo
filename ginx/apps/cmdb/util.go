c02493b88784bdafdf8bd98e7ae6a1f963de6800
fengzhongzhu1621 <jinyinqiao@gmail.com>
2025-03-18 23:21:43 +0800

    feat: add ParseModelBizID func

---
+++ ginx/apps/cmdb/util.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"encoding/json"
+)
+
+func ParseModelBizID(data string) (int64, error) {
+	model := &struct {
+		BizID int64 `json:"bk_biz_id"`
+	}{}
+
+	if len(data) != 0 {
+		if err := json.Unmarshal([]byte(data), model); nil != err {
+			return 0, err
+		}
+	}
+
+	return model.BizID, nil
+}
