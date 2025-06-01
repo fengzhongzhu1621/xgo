1a4ca46d4495b8118afb10e7a7578a74d99c0e87
fengzhongzhu1621 <jinyinqiao@gmail.com>
2025-06-01 22:43:41 +0800

    test: add reflect unittest

--- buildin/reflectutils/interface_test.go
+++ buildin/reflectutils/interface_test.go
@@ -14,3 +14,13 @@ func TestInterface(t *testing.T) {
 	fmt.Println("Type:", t1)             // Type: float64
 	fmt.Println("Value:", v.Interface()) // Value: 3.4
 }
+
+// 反射允许在运行时对接口变量进行类型断言
+func TestInterfaceAny(t *testing.T) {
+	var i interface{} = "hello"
+	v := reflect.ValueOf(i)
+
+	if s, ok := v.Interface().(string); ok {
+		fmt.Println(s) // hello
+	}
+}
