2b419578fe790227c3f239d9553f0ff280195b18
fengzhongzhu1621 <jinyinqiao@gmail.com>
2025-06-14 12:24:31 +0800

    feat: add ParseCommandlineMap func

--- str/stringutils/split_test.go
+++ str/stringutils/split_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/duke-git/lancet/v2/strutil"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 // TestHead 根据分隔符分割字符串.
@@ -404,4 +405,58 @@ func TestFieldsFuncSeq(t *testing.T) {
 	// "abc"
 	// "def"
 	// "ghi"
+}
+
+func TestParseInputMapping(t *testing.T) {
+	var src string
+	var expected, parsed map[string]string
+	var err error
+
+	src = "key1:value1,key2:value2"
+	expected = map[string]string{"key1": "value1", "key2": "value2"}
+	parsed, err = ParseCommandlineMap(src)
+	require.NoError(t, err)
+	assert.Equal(t, expected, parsed)
+
+	src = `key1:"value1,value2",key2:value3`
+	expected = map[string]string{"key1": "value1,value2", "key2": "value3"}
+	parsed, err = ParseCommandlineMap(src)
+	require.NoError(t, err)
+	assert.Equal(t, expected, parsed)
+
+	src = `key1:"value1,value2,key2:value3"`
+	expected = map[string]string{"key1": "value1,value2,key2:value3"}
+	parsed, err = ParseCommandlineMap(src)
+	require.NoError(t, err)
+	assert.Equal(t, expected, parsed)
+
+	src = `"key1,key2":value1`
+	expected = map[string]string{"key1,key2": "value1"}
+	parsed, err = ParseCommandlineMap(src)
+	require.NoError(t, err)
+	assert.Equal(t, expected, parsed)
+}
+
+func TestSplitString(t *testing.T) {
+	var src string
+	var expected, result []string
+	var err error
+
+	src = "1,2,3"
+	expected = []string{"1", "2", "3"}
+	result = SplitStringEx(src, ',')
+	require.NoError(t, err)
+	assert.Equal(t, expected, result)
+
+	src = `"1,2",3`
+	expected = []string{`"1,2"`, "3"}
+	result = SplitStringEx(src, ',')
+	require.NoError(t, err)
+	assert.Equal(t, expected, result)
+
+	src = `1,"2,3",`
+	expected = []string{"1", `"2,3"`, ""}
+	result = SplitStringEx(src, ',')
+	require.NoError(t, err)
+	assert.Equal(t, expected, result)
 }
