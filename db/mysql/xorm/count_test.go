c16bd86caa753badb6d009358797b42a934b2ad2
fengzhongzhu1621 <jinyinqiao@gmail.com>
2025-06-30 18:02:21 +0800

    test: add TestCountUseRawSql for xorm

--- db/mysql/xorm/count_test.go
+++ db/mysql/xorm/count_test.go
@@ -22,3 +22,16 @@ func TestCount(t *testing.T) {
 	count, err := xormEngine.Where("name = ?", "bob").Count(cardM)
 	assert.Equal(t, true, count >= 0)
 }
+
+func TestCountUseRawSql(t *testing.T) {
+	dbClient := GetDefaultXormDBClient()
+	xormEngine := dbClient.DB
+
+	var count int64
+	has, err := xormEngine.SQL("SELECT COUNT(*) FROM xorm_card_m").Get(&count)
+	if err != nil {
+		panic(err)
+	}
+	assert.Equal(t, true, has)
+	assert.Equal(t, true, count >= 0)
+}
