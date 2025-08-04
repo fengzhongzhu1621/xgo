22ab903a5c7485e0170fca04410eebdafdd409e7
fengzhongzhu1621 <jinyinqiao@gmail.com>
2025-08-04 17:43:14 +0800

    feat: add hex func

--- crypto/hex/hex.go
+++ crypto/hex/hex.go
@@ -23,3 +23,17 @@ func NewScript(src string) *Script {
 func (s *Script) Hash() string {
 	return s.hash
 }
+
+// FromHex is a wrapper around hex.Decode to support go1.20
+func FromHex(dst []byte, src string) (int, error) {
+	return hex.Decode(dst, []byte(src))
+}
+
+// AppendEncode appends the hex-encoded src to dst and returns the extended
+// buffer.
+func AppendEncode(dst, src []byte) []byte {
+	d := make([]byte, len(src)*2)
+	hex.Encode(d, src)
+	dst = append(dst, d...)
+	return dst
+}
